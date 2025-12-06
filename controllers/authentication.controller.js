aa499279f4b2d893e277ce652a10d9953300c8e8
abdullahkhan16122008 <abdullahkhan16122008@gmail.com>
2025-12-06 20:25:19 +0500

    a

--- controllers/authentication.controller.js
+++ controllers/authentication.controller.js
@@ -75,15 +75,15 @@ let loginController = async (req, res) => {
     // Cookies set karo
     res.cookie('access_token', accessToken, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        sameSite: 'strict',
+        secure: true,
+        sameSite: 'none',
         maxAge: 15 * 60 * 1000 // 15 minutes
     });
 
     res.cookie('refresh_token', refreshToken, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        sameSite: 'strict',
+        secure: true,
+        sameSite: 'none',
         maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
     });
 
